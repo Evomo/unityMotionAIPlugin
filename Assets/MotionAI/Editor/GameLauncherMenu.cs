7fc136007835383085fe88235796f324582a73bb
Dan Amador <danamadorpe@gmail.com>
2020-08-25 20:49:24 +0200

    Added scene references and scriptable object

---
+++ Assets/MotionAI/Editor/GameLauncherMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Threading;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+using PackageInfo = UnityEditor.PackageInfo;
+
+namespace MotionAI.Editor {
+	public class GameLauncherMenu : MonoBehaviour {
+		[MenuItem("Evomo/Create Game Asset")]
+		public static void CreateSceneObjects() {
+			GameLauncherData gs = ScriptableObject.CreateInstance<GameLauncherData>();
+			gs.gameName = Application.productName;
+
+			AssetDatabase.CreateAsset(gs, $"Assets/{gs.gameName}.asset");
+
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
+		}
+	}
+}
+
---
+++ Assets/MotionAI/GameLauncherData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MotionAI {
+		public class GameLauncherData : ScriptableObject {
+			public SceneReference mainSceneReference;
+			public string gameName;
+			public Texture2D image;
+			public string description;
+			
+		}
+	}
