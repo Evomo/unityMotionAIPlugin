d4c80acb81aad841fcda05b0fb7c4e077e34ee80
Jakob Wowy <jakob.wowy@evomo.de>
2020-09-18 17:26:30 +0200

    adjust to bridge v2

--- Assets/MotionAI/Core/MotionAIManager.cs
+++ Assets/MotionAI/Core/MotionAIManager.cs
@@ -3,17 +3,22 @@ using AOT;
 #endif
 using System;
 using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using MotionAI.Core.Controller;
 using MotionAI.Core.Models.Constants;
 using MotionAI.Core.POCO;
 using UnityEngine;
 using static MotionAI.UtilHelper;
 
-namespace MotionAI.Core {
-	public class MotionAIManager : MonoBehaviour {
-		#region Internal Load
+namespace MotionAI.Core
+{
+    public class MotionAIManager : MonoBehaviour
+    {
+        #region Internal Load
 
-		public delegate void UnityCallback(string value);
+        public delegate void UnityCallback(string value);
 
 #if UNITY_IOS && !UNITY_EDITOR
     [DllImport("__Internal")]
@@ -39,10 +44,10 @@ namespace MotionAI.Core {
 
 #endif
 
-		#endregion
+        #endregion
 
 
-		#region Bridge Methods
+        #region Bridge Methods
 
 #if UNITY_IOS && !UNITY_EDITOR
     [MonoPInvokeCallback(typeof(UnityCallback))]
@@ -54,101 +59,115 @@ namespace MotionAI.Core {
 #endif
 
 
-		public void StartTracking() {
+        public void StartTracking()
+        {
 #if UNITY_IOS && !UNITY_EDITOR
-# TODO: Add third parameter gaming - if model_type == gaming -> input_string = "true"
-        StartEvomoBridge(UtilHelper.ToCustomOrientation(Input.deviceOrientation), mySDKConfig.classificationModel.ToString, "true");
+// TODO: Add third parameter gaming - if model_type == gaming -> input_string = "true"
+// UtilHelper.ToCustomOrientation(Input.deviceOrientation)
+        StartEvomoBridge("buttonDown", mySDKConfig.classificationModel.ToString(), "true");
 #endif
-			IsTracking = true;
-			ControlPairing();
-		}
+            IsTracking = true;
+            ControlPairing();
+        }
 
-		public void StopTracking() {
+        public void StopTracking()
+        {
 #if UNITY_IOS && !UNITY_EDITOR
         StopEvomoBridge();
 #endif
-			IsTracking = false;
-		}
+            IsTracking = false;
+        }
 
-		public static void LogEvent(string eventType, string note = "") {
+        public static void LogEvent(string eventType, string note = "")
+        {
 #if UNITY_IOS && !UNITY_EDITOR
         LogEventBridge(eventType, note);
 #endif
-		}
+        }
 
-		public static void LogTargetMovement(ElmoEnum movementType, string note = "") {
+        public static void LogTargetMovement(ElmoEnum movementType, string note = "")
+        {
 #if UNITY_IOS && !UNITY_EDITOR
         LogTargetMovementBridge(movementType.ToString(), note);
 #endif
-		}
+        }
 
-		public void LogFailure(EventSource source, FailureType failureType, ElmoEnum movementType,
-			string note = "") {
+        public void LogFailure(EventSource source, FailureType failureType, ElmoEnum movementType,
+            string note = "")
+        {
 #if UNITY_IOS && !UNITY_EDITOR
         LogFailureBridge(source.ToString(), failureType.ToString(), movementType.ToString(), note);
 #endif
-		}
+        }
 
-		public void SetUsername(String username) {
+        public void SetUsername(String username)
+        {
 #if UNITY_IOS && !UNITY_EDITOR
         SetUsernameBridge(username);
 #endif
-		}
+        }
 
-		#endregion
+        #endregion
 
 
-		#region Unity
+        #region Unity
 
-		public SDKConfig mySDKConfig;
-		public ControllerManager controllerManager;
+        public SDKConfig mySDKConfig;
+        public ControllerManager controllerManager;
 
-		public bool IsTracking { get; private set; }
+        public bool IsTracking { get; private set; }
 
-		#region Lifecycle
+        #region Lifecycle
 
-		private void Awake() {
+        private void Awake()
+        {
 #if UNITY_IOS && !UNITY_EDITOR
         SetUsernameBridge(mySDKConfig.username);
 #endif
-			controllerManager = new ControllerManager();
-		}
+            controllerManager = new ControllerManager();
+        }
 
-		private void OnEnable() {
+        private void OnEnable()
+        {
 #if UNITY_IOS && !UNITY_EDITOR
-				# TODO add parameter to global Manager to define if debuggin is active (sdk will send some debugging and raw measurement data to the server)
-				# Enter the boolean as string like "true" and "false"
+				// TODO add parameter to global Manager to define if debuggin is active (sdk will send some debugging and raw measurement data to the server)
+				// Enter the boolean as string like "true" and "false"
         InitEvomoBridge(MessageRecived, mySDKConfig.licenseID, "true");
 #endif
-		}
+        }
 
 
-		private void OnDisable() {
-			StopTracking();
-		}
+        private void OnDisable()
+        {
+            StopTracking();
+        }
 
-		#endregion
-
-
-		public void ManageMotion(string movementStr) {
-			BridgeMessage msg = JsonUtility.FromJson<BridgeMessage>(movementStr);
+        #endregion
 
 
-			if (msg.movement == null) {
-				Movement mv = new Movement();
-				mv.elmos.Add(msg.elmo);
-				controllerManager.ManageMotion(mv);
-			}
-			else {
-				controllerManager.ManageMotion(msg.movement);
-			}
-		}
+        public void ManageMotion(string movementStr)
+        {
+            BridgeMessage msg = JsonUtility.FromJson<BridgeMessage>(movementStr);
 
 
-		public void ControlPairing() {
-			controllerManager.PairController(FindObjectsOfType<MotionAIController>().ToList());
-		}
+            if (msg.movement == null)
+            {
+                Movement mv = new Movement();
+                mv.elmos.Add(msg.elmo);
+                controllerManager.ManageMotion(mv);
+            }
+            else
+            {
+                controllerManager.ManageMotion(msg.movement);
+            }
+        }
 
-		#endregion
-	}
+
+        public void ControlPairing()
+        {
+            controllerManager.PairController(FindObjectsOfType<MotionAIController>().ToList());
+        }
+
+        #endregion
+    }
 }
