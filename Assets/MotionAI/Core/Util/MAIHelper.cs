fd786e7aa2780e82a4a849e01bd2e7572603c0c6
Dan Amador <DanAmador@users.noreply.github.com>
2020-12-11 11:53:10 +0100

    fix: logs

---
+++ Assets/MotionAI/Core/Util/MAIHelper.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace MotionAI.Core.Util {
+    public class MAIHelper {
+        public static void Log(string message) {
+            Debug.Log($"MotionAI: {message}");
+        }
+    }
+}
